e6e1c50695e3cd1f43bc0d48475a93ed05741410
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2017-01-18 15:16:53 -0600

    Add enough logic to the code to parse the list of ENCODE motifs to get the full, correct list of transcription factor prefixes.

--- motif-lookups/encode/encode-motif-lut.py
+++ motif-lookups/encode/encode-motif-lut.py
@@ -2,20 +2,20 @@ import re
 import pickle
 
 
-tf_map_file = 'encode-prefixes.txt' 
+tf_map_file = 'encode-motifs.txt' 
 with open(tf_map_file) as f:
     lines = f.readlines()
 
 lut_encode_prefixes = {} 
 
 for line in lines:
-    matchObj = line.split(" ") 
-    prefix = matchObj[0]
-    print "prefix " + prefix
-    lut_encode_prefixes[prefix] = prefix 
-    
-
-
+    lineMatch = re.match('^\>(\w+)_+.+', line)
+    if lineMatch is not None: 
+      #matchObj = line.split(" ") 
+      prefix = lineMatch.group(1)
+      print "prefix " + prefix
+      if not prefix in  lut_encode_prefixes:
+          lut_encode_prefixes[prefix] = prefix 
 
 def save_obj(obj, name ):
     with open(name + '.pkl', 'wb') as f:
