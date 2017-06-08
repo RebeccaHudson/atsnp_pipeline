87d66059c4275954ca7f8385f8aec712b2bc0e19
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2017-06-08 16:19:09 -0500

    Put a class for identifying, parsing, etc names of one or more transcription factor library(/ies) from the command line; shared between the scripts setup_jobs_and_dirs and collect_progress_info.

--- primary-data-pipeline/shared_pipe.py
+++ primary-data-pipeline/shared_pipe.py
@@ -49,4 +49,39 @@ def init():
    #condor sumibt files get put in primary_pipeline/encode
    #
 
-    
+   
+#Add another transcription factor library down here too. 
+class WhichTFLibs:
+    #If msg_for_no_args is None or left off, then there is 
+    #no error message displayed and it is assumed both data sets
+    #are being queried.
+    valid_args = ['encode', 'jaspar']
+
+    def __init__(self, cmd_args, msg_for_no_args=None):
+        self.run_these = self.which_datasets(cmd_args, msg_for_no_args) 
+
+    def which_datasets(self, cmd_args, msg_for_no_args):
+        run_these = []
+        #print "sys.argv  " + repr(cmd_args)
+        #print "len(sys.argv)" +  str(len(cmd_args))
+        if len(cmd_args) >= 2 and len(cmd_args) <= 3:
+           sets_to_run = cmd_args[1:]
+           while len(sets_to_run) > 0:
+               g = sets_to_run.pop()
+               if str.lower(g) not in self.valid_args: 
+                   msg = "The only valid arguments are 'encode' and/or 'jaspar'\
+                       (case insensitive). \n Enter 'jaspar' or 'encode' or both."
+                   print msg
+                   exit(1)
+               else:
+                   run_these.append(str.lower(g))
+        if len(run_these) == 0:
+            if msg_for_no_args is not None:
+                print msg_for_no_args
+                exit(1)
+            run_these = self.valid_args 
+        return run_these
+
+
+
+
