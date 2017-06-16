35548b42ef6bc66c9117b9a39231d883a9f91ef2
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2017-06-16 14:57:38 -0500

    Parametrize and organize a little bit.

--- snp-info-pipeline/shared_pipe.py
+++ snp-info-pipeline/shared_pipe.py
@@ -1,10 +1,15 @@
 #shared_pipe.py
 #stuff shared by all the pipeline scripts.
 def init():
-    global SETTINGS, PROGRESS_STATES
+    global SETTINGS, PROGRESS_STATES, SNP_INFO_PARENT_DIR, DRY_RUN
+
+    DRY_RUN = False    #If trye, don't put data into Elasticsearch.
+
+    #These settings can be left alone. Tuned pretty well for SNP-INFO.
     SETTINGS = {
          'chunk_count': 10,
-         'n_submit_files' : 2 #number of separate condor submit files.
+         'n_submit_files' : 2, #number of separate condor submit files.
+         'index_name' : 'snp_info_test_1'
     }  
 
     #Should match the definition in the per-job scripts;
@@ -13,3 +18,5 @@ def init():
                         'IN_PROGRESS': 1,
                         'COMPLETE'   : 2  
                        } 
+
+    SNP_INFO_PARENT_DIR = '/z/Comp/kelesgroup/atsnp/SNP_INFO'
